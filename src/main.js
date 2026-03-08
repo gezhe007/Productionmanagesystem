c7a8b24edf8759b4a0d48556103e78fd4fabf85b
gezhe007 <3542697285@qq.com>
2026-03-08 15:26:00 +0800

    perf: 按需引入 Element UI 组件，减小打包体积

--- src/main.js
+++ src/main.js
@@ -2,10 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
+import { Button, Input, Collapse, DatePicker, InputNumber, Dialog, Form, FormItem, Card, Alert, Row, Col, Empty, Tag, Select, Option, CollapseItem,Message } from 'element-ui'
+Vue.component(Button.name, Button)
+Vue.component(Input.name, Input)
+Vue.component(Collapse.name, Collapse)
+Vue.component(DatePicker.name, DatePicker)
+Vue.component(InputNumber.name, InputNumber)
+Vue.component(Dialog.name, Dialog)
+Vue.component(Form.name, Form)
+Vue.component(FormItem.name, FormItem)
+Vue.component(Card.name, Card)
+Vue.component(Alert.name, Alert)
+Vue.component(Row.name, Row)
+Vue.component(Col.name, Col)
+Vue.component(Empty.name, Empty)
+Vue.component(Tag.name, Tag)
+Vue.component(Select.name, Select)
+Vue.component(Option.name, Option)
+Vue.component(CollapseItem.name, CollapseItem)
+Vue.prototype.$message = Message
 
 new Vue({
   router,
